c254c77f759322dea10d46994f3711d1fefce538
Ha Chanh Nam B1909951 <chanhnam10@gmail.com>
2022-09-22 11:03:42 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
-const contactsRouter = require("./app/routes/contact.route");
+const contactsRouter = require("../contactbook-backend/app/routes/contact.route");
+const ApiError = require("./app/api-error");
 const app = express();
 
 app.use(cors());
@@ -9,4 +10,12 @@ app.get("/", (req, res) => {
     res.json({ message: "Welcome to contact book application." });
 });
 app.use("/api/contacts", contactsRouter);
+app.use((req, res, next) => {
+    return next(new ApiError(404, "Resoure not found"));
+});
+app.use((error, req, res, next) => {
+    return res.status(error.statusCode || 500).json({
+        message: error.message || "Internal Sever Error",
+    });
+});
 module.exports = app;
